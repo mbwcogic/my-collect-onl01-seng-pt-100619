80728de823ac723ccd383ad2d6c74e61a8364fab
Michael Waldo <mbwcogic@yahoo.com>
2019-10-21 00:50:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+#def my_collect(empty_array) 
+  #i = 0
+  #while i < empty_array.length
+  #yield (my_collect.upcase) 
+  #i +=1 
+#end
 
-
+#end
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
