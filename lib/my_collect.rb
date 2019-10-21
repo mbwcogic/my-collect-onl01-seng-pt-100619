d877ab87fc9f481a6826776a6102fe6e1090e5fc
Michael Waldo <mbwcogic@yahoo.com>
2019-10-21 00:36:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(empty_array) 
   i = 0
   while i < empty_array.length
-  yield my_collect(collection) do |lang|
+  yield my_collect(collection) |lang|
     lang.upcase
   i +=1 
 end
