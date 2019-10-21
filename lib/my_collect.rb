a883b5ca8c7876d8de271bf690b2d324c829157d
Michael Waldo <mbwcogic@yahoo.com>
2019-10-21 00:27:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 def my_collect(empty_array) 
   i = 0
   while i < empty_array.length
-  
+  my_collect(array) do |name|
+  name.split(" ").first
   i +=1 
 end
 i 
 end
 
-my_collect(array) do |name|
-  name.split(" ").first
+
 end
